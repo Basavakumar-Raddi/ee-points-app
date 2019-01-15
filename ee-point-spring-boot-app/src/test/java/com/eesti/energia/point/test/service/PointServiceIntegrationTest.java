c4521c313a1ff451cfaa889889360f64c6dd5ad0
Basavakumar-Raddi <30400312+Basavakumar-Raddi@users.noreply.github.com>
2019-01-15 17:49:06 +0530

    added the integration test with h2 database

--- ee-point-spring-boot-app/src/test/java/com/eesti/energia/point/test/service/PointServiceIntegrationTest.java
+++ ee-point-spring-boot-app/src/test/java/com/eesti/energia/point/test/service/PointServiceIntegrationTest.java
@@ -3,14 +3,20 @@ package com.eesti.energia.point.test.service;
 import java.time.LocalTime;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorCompletionService;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicReference;
 
-import javax.inject.Inject;
+import javax.validation.constraints.AssertTrue;
 
-import com.eesti.energia.point.dao.PointRepository;
+import com.eesti.energia.point.PointApplication;
 import com.eesti.energia.point.dto.PointDTO;
 import com.eesti.energia.point.service.PointService;
 import com.eesti.energia.point.util.LocationEnum;
@@ -19,97 +25,92 @@ import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.util.CollectionUtils;
 
+import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.MOCK;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(classes = PointApplication.class)
+@ComponentScan("com.eesti.energia.point")
+@TestPropertySource(locations = "classpath:application-integrationtest.properties")
 public class PointServiceIntegrationTest {
 
     @Autowired
     private PointService pointService;
-    @Autowired
-    private PointRepository pointRepository;
-
 
     @Test
-    public void testAddSequential() {
-        PointDTO pointDto =  new PointDTO();
-        pointDto.setLocation(LocationEnum.LV);
-        pointDto.setMeasurementDay("2088-02-29");
-        pointDto.setValue(15.15);
-        PointDTO pointDTOResp = null;
-
-        try {
-            pointDTOResp = pointService.addPoint(pointDto);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
+    public void testAddSequential() throws InterruptedException, ExecutionException {
+        PointDTO pointDto =  PointDTO.builder()
+                .location(LocationEnum.LV)
+                .measurementDay("2088-02-29")
+                .value(15.15).build();
+        addPoint(pointDto);
+        List<Double> expectedValues = Arrays.asList(30.30, 45.45);
+        ExecutorService pool = Executors.newFixedThreadPool(3);
+        ExecutorCompletionService es = new ExecutorCompletionService(pool);
+        es.submit(() -> updatePointThrd1(pointDto));
+        es.submit(() -> updatePointThrd3(pointDto));
+        List<Double> expectedPointValues = new ArrayList<>();
+        for(int i=0;i<2;i++) {
+            Future<PointDTO> f = es.take();
+            expectedPointValues.add(f.get().getValue());
         }
-
-        ExecutorService es = Executors.newFixedThreadPool(3);
-        try {
-            es.execute(() -> {
-                PointDTO firstThreadResp = updatePointThrd1(pointDto);
-                Assert.assertEquals(firstThreadResp.getValue(),Double.valueOf("30.30"));
-            });
-            es.execute(() -> {
-                //simulating other user by using different thread
-                PointDTO secondThreadResp = updatePointThrd2(pointDto);
-                Assert.assertEquals(secondThreadResp.getValue(),Double.valueOf("45.45"));
-            });
-            es.shutdown();
-            es.awaitTermination(2, TimeUnit.MINUTES);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }finally {
-            if(pointDTOResp!=null){
-                pointRepository.deleteById(pointDTOResp.getId());
-            }
-        }
-
+        Assert.assertEquals(2, expectedPointValues.size());
+        Assert.assertTrue(expectedValues.containsAll(expectedPointValues));
+        pool.shutdown();
     }
 
     @Test
-    public void testAddConcurrent() {
-        List values = new ArrayList();
-        values.add(20.22);
-        values.add(30.33);
-        PointDTO pointDto =  new PointDTO();
-        pointDto.setLocation(LocationEnum.EE);
-        pointDto.setMeasurementDay("2088-02-29");
-        pointDto.setValue(10.11);
-
-        PointDTO pointDTOResp = null;
-        try {
-            pointDTOResp = pointService.addPoint(pointDto);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
+    public void testAddConcurrent() throws ExecutionException, InterruptedException {
+        PointDTO pointDto =  PointDTO.builder()
+                                        .location(LocationEnum.EE)
+                                        .measurementDay("2088-01-29")
+                                        .value(10.11).build();
+        addPoint(pointDto);
+        List<Double> expectedValues = Arrays.asList(20.22, 30.33);
+        ExecutorService pool = Executors.newFixedThreadPool(3);
+        ExecutorCompletionService es = new ExecutorCompletionService(pool);
+        es.submit(() -> updatePointThrd1(pointDto));
+        es.submit(() -> updatePointThrd3(pointDto));
+        List<Double> expectedPointValues = new ArrayList<>();
+        for(int i=0;i<2;i++) {
+            Future<PointDTO> f = es.take();
+            expectedPointValues.add(f.get().getValue());
         }
+        Assert.assertEquals(2, expectedPointValues.size());
+        Assert.assertTrue(expectedValues.containsAll(expectedPointValues));
+        pool.shutdown();
+    }
 
-        ExecutorService es = Executors.newFixedThreadPool(3);
-        try {
-            es.execute(() -> {
-                PointDTO firstThreadResp = updatePointThrd1(pointDto);
-                Assert.assertTrue(values.contains(firstThreadResp.getValue()));
-                values.remove(firstThreadResp.getValue());
-            });
-            es.execute(() -> {
-                //simulating other request by using different thread
-                PointDTO secondThreadResp = updatePointThrd3(pointDto);
-                Assert.assertTrue(values.contains(secondThreadResp.getValue()));
-            });
-            es.shutdown();
-            es.awaitTermination(2, TimeUnit.MINUTES);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        } finally {
-            if(pointDTOResp!=null){
-                pointRepository.deleteById(pointDTOResp.getId());
-            }
+    @Test
+    public void testAddFailure() throws ExecutionException, InterruptedException {
+        PointDTO pointDto =  PointDTO.builder()
+                .location(LocationEnum.EE)
+                .measurementDay("2088-02-29")
+                .value(10.11).build();
+        addPoint(pointDto);
+        List<Double> expectedValues = Arrays.asList(100.12, 300.45);
+        ExecutorService pool = Executors.newFixedThreadPool(3);
+        ExecutorCompletionService es = new ExecutorCompletionService(pool);
+        es.submit(() -> updatePointThrd1(pointDto));
+        es.submit(() -> updatePointThrd3(pointDto));
+        List<Double> expectedPointValues = new ArrayList<>();
+        for(int i=0;i<2;i++) {
+            Future<PointDTO> f = es.take();
+            expectedPointValues.add(f.get().getValue());
         }
+        Assert.assertNotEquals(1, expectedPointValues.size());
+        Assert.assertFalse(expectedValues.containsAll(expectedPointValues));
+        pool.shutdown();
+    }
 
-
+    private void addPoint(final PointDTO pointDTO) throws InterruptedException {
+        pointService.addPoint(pointDTO);
     }
 
     private PointDTO updatePointThrd1(PointDTO pointDto) {
@@ -124,7 +125,7 @@ public class PointServiceIntegrationTest {
 
     private PointDTO updatePointThrd2(PointDTO pointDto) {
         PointDTO foundPoint = null;
-        try {
+        try {//some delay before adding
             TimeUnit.MILLISECONDS.sleep(3000);
         } catch (InterruptedException e) {
             e.printStackTrace();
