47c5ef65912490ca165e8de963864fccabeed1b4
Basavakumar-Raddi <30400312+Basavakumar-Raddi@users.noreply.github.com>
2019-01-17 12:32:54 +0530

    added for junit

--- ee-point-spring-boot-app/src/main/java/com/eesti/energia/point/dto/PointDTO.java
+++ ee-point-spring-boot-app/src/main/java/com/eesti/energia/point/dto/PointDTO.java
@@ -2,11 +2,18 @@ package com.eesti.energia.point.dto;
 
 import com.eesti.energia.point.util.LocationEnum;
 
+import jdk.nashorn.internal.objects.annotations.Constructor;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class PointDTO {
 
     private String id;
